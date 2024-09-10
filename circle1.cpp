ebbc83edade851a4a707c909581a6ae2e0a81af9
abdultechs <abdmaqsood6@gmail.com>
2024-09-10 08:34:27 -0700

    message

---
+++ circle1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main(){
+cout << "    aaa"<< endl;
+cout << "  a     a"<< endl;
+cout << " a       a"<< endl;
+cout << "  a     a"<< endl;
+cout << "    aaa"<< endl; 
+
+
+}
